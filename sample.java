aa9f00894ac100df000b1e0a09558660c1900caf
Akshay Khajuriya <241728994+cx-akshay-khajuriya@users.noreply.github.com>
2026-07-14 14:33:49 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 public class Sample {
     public static void main(String[] args) throws IOException {
-        //!! Hardcdoded dsdsccxcccredentia;l lSAST finding  zxczx!!sa
+        //!! Hardcdoded dsdsccxcccredentia;l lSAST finding  zxczx!!sa!!
         String password = "sensitivePassword";
         System.out.println("Received password: " + password);
         // Command Injection (SAST finding)[
